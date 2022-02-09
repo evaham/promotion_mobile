20f3fd8542e0d9560f0031bb9990d4a4c4f6a935
JungWoo Ham <evaham@naver.com>
2022-02-09 15:30:05 +0900

    20220209

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,125 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+    <div class="section">
+      <!-- <h1>투게더 POS 본부시스템</h1> -->
+      <h2>투게더 POS <b>본부시스템</b>이란?</h2>
+      <ul class="infolist">
+        <li>
+          <p>
+            <em>01</em>
+            다점포 매장용 관리 솔루션
+            <span><img src="@/assets/mart_link.png" /></span>
+          </p>
+        </li>
+        <li>
+          <p>
+            <em>02</em>
+            설치가 필요 없는 웹 기반 솔루션으로 <br />
+            어디서든 접근 가능 (크롬, 엣지 권장)
+            <span><img src="@/assets/browser.png" /></span>
+          </p>
+        </li>
+        <li>
+          <p class="center bolder">
+            매장 운영상황에 맞게 <br />
+            2가지 버전 중 채택 가능
+          </p>
+          <div class="cardbox">
+            <div>
+              <p>미통합 버전</p>
+              <p>
+                매장 DB 통합 없이 <br />
+                기존대로 사용 가능
+              </p>
+              <p>매입, 매출을 한눈에 조회할 수 있는 화면 제공</p>
+            </div>
+            <div>
+              <p>통합버전</p>
+              <p>전체매장 DB 통합(상품, 분류, 공급사) 필요</p>
+              <p>
+                전체매장의 상품, 공급사, 분류 데이터를 통합하여 조회, 관리할 수
+                있다.
+              </p>
+            </div>
+          </div>
+        </li>
+      </ul>
+    </div>
+    <div class="section">
+      <h2>투게더 POS 본부시스템 <b>신청 절차</b></h2>
+      <ul class="progresslist">
+        <li>
+          <p>
+            <em>1</em>
+            <span>본부시스템 통합/미통합 버전 확정</span>
+          </p>
+        </li>
+        <li>
+          <p>
+            <em>2</em>
+            <span
+              >도입 희망일 선택 <br />
+              <small>(통합 여부에 따라 매장 상품장 데이터 검수 필요)</small>
+            </span>
+          </p>
+        </li>
+        <li>
+          <p>
+            <em>3</em>
+            <span>접속 도메인(URL) 결정</span>
+          </p>
+        </li>
+        <li>
+          <p>
+            <em>4</em>
+            <span>도메인 생성, 본부 DB구성, 웹페이지 배포</span>
+          </p>
+        </li>
+        <li>
+          <p>
+            <em>5</em>
+            <span
+              >데이터 검수 완료 후 매장 데이터 적용 일자 확정 및 파트너사에서
+              로컬서버 셋팅</span
+            >
+          </p>
+        </li>
+        <li>
+          <p>
+            <em>6</em>
+            <span>판매 시작일 각 매장 데이터의 정상적 수집 확인</span>
+          </p>
+        </li>
+        <li>
+          <p>
+            <em>7</em>
+            <span
+              >본부 그룹에 매장이 추가되는 경우 각 매장의 파일자료실 본부 DB로
+              로컬서버 셋팅</span
+            >
+          </p>
+        </li>
+      </ul>
+    </div>
+    <div class="section">
+      <h2>투게더 POS <b>본부시스템 비용</b></h2>
+      <div class="costbox">
+        <p>
+          <span>DB 검수 및 도입비</span>
+          <span>매장당 10 만원</span>
+        </p>
+        <p>
+          <span>월 사용료</span>
+          <span>OPR 카피당 2만원</span>
+        </p>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld,
-  },
+  components: {},
 };
 </script>
