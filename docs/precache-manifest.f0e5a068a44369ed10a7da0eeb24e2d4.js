230e03e759f257227e65766455e2032b2348249a
JungWoo Ham <evaham@naver.com>
2022-02-09 17:22:16 +0900

    111

---
+++ docs/precache-manifest.f0e5a068a44369ed10a7da0eeb24e2d4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8f6d416e2cd6722b5fa",
+    "url": "promotion_mobile/css/app.35326795.css"
+  },
+  {
+    "revision": "4d6eabc07f3d0d1b3cbe8f8aac9ae8f5",
+    "url": "promotion_mobile/index.html"
+  },
+  {
+    "revision": "93ed52732dcaa95370e1",
+    "url": "promotion_mobile/js/about.6524b863.js"
+  },
+  {
+    "revision": "a8f6d416e2cd6722b5fa",
+    "url": "promotion_mobile/js/app.f7be3bab.js"
+  },
+  {
+    "revision": "6b15893a953fcef9b03a",
+    "url": "promotion_mobile/js/chunk-vendors.21426927.js"
+  },
+  {
+    "revision": "c31e5239b9503129d3d17cc8f7f2a9a8",
+    "url": "promotion_mobile/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "promotion_mobile/robots.txt"
+  }
+]);
---
+++ vue.config.js
@@ -0,0 +1,4 @@
+module.exports = {
+  outputDir:'./docs',
+  publicPath: 'promotion_mobile'
+}
